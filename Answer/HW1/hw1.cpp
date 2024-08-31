80ab6ebaf729d16e559f081c478de52a41fb99d6
YouZhiZheng <youzhizheng9@gmail.com>
2024-08-31 10:04:27 +0800

    refactor(Answer/hw1.cpp):refactored this code

--- Answer/HW1/hw1.cpp
+++ Answer/HW1/hw1.cpp
@@ -1,271 +1,401 @@
 #include "hw1.h"
 
-Matrix algebra::zeros(size_t n, size_t m) {
-    return Matrix(n, std::vector<double>(m, 0));
+Matrix algebra::zeros(size_t n, size_t m)
+{
+    return Matrix(n, std::vector<double>(m, 0.0));
 }
 
-Matrix algebra::ones(size_t n, size_t m) {
-    return Matrix(n, std::vector<double>(m, 1));
+Matrix algebra::ones(size_t n, size_t m)
+{
+    return Matrix(n, std::vector<double>(m, 1.0));
 }
 
-Matrix algebra::random(size_t n, size_t m, double min, double max) {
-    if(min > max) {
-        throw std::logic_error(" min number should less than max number");
+
+Matrix algebra::random(size_t n, size_t m, double min, double max)
+{
+    if(min > max)
+    {
+        throw std::logic_error("min is greater than max");
     }
-    std::default_random_engine generator;
+
+    Matrix matrix(n, std::vector<double>(m, 0.0));
+    
+    std::random_device rd;
+    std::mt19937 generator(rd());
     std::uniform_real_distribution<double> distribution(min, max);
-    Matrix matrix(n, std::vector<double>(m));
-    for(auto &row : matrix) {
-        for(auto &elem : row) {
-            elem = distribution(generator);
+
+    for(size_t i = 0; i < n; ++i)
+    {
+        for(size_t j = 0; j < m; ++j)
+        {
+            matrix[i][j] = distribution(generator);
         }
     }
+
     return matrix;
 }
 
-void algebra::show(const Matrix& matrix) {
-    std::cout << std::fixed << std::setprecision(3);
-    for(auto row : matrix) {
-        for(auto element: row) {
-            std::cout << element << " ";
+void algebra::show(const Matrix& matrix)
+{
+    if(matrix.empty()) return;
+
+    for(const std::vector<double> &row: matrix)
+    {
+        for(const double element: row)
+        {
+            std::cout << std::setw(12) << std::left << element; 
         }
+
         std::cout << std::endl;
     }
 }
 
-Matrix algebra::multiply(const Matrix& matrix, double c) {
-    Matrix new_matrix = matrix;
-    for(auto &row : new_matrix) {
-        for(auto &elem : row) {
-            elem *= c;
-        }
-    }
-    return new_matrix;
-}
 
-Matrix algebra::multiply(const Matrix& matrix1, const Matrix& matrix2) {
-    if(matrix1.empty() || matrix2.empty()) {
-        if(matrix1.empty() && matrix2.empty()) {
-            Matrix matrix;
-            return matrix;
-        }
-        throw std::logic_error("cannot multiply a non-empty matrix with an empty matrix");
-    }
-    if(matrix1[0].size() != matrix2.size()) {
-        throw std::logic_error("cannot multipy two different dimension matrixs");
-    }
-    int row_num = matrix1.size();
-    int column_num = matrix2[0].size();
-    int middle_layer = matrix1[0].size();
-    Matrix matrix(row_num, std::vector<double>(column_num));
-    for(int i = 0; i < row_num; ++i) {
-        for(int j = 0; j < column_num; ++j) {
-            for(int k = 0; k < middle_layer; ++k) {
-                matrix[i][j] += matrix1[i][k] * matrix2[k][j];
+Matrix algebra::multiply(const Matrix& matrix, double c)
+{
+    Matrix m_matrix(matrix);
+
+    if(!m_matrix.empty())
+    {
+        for(std::vector<double> &row: m_matrix)
+        {
+            for(double &element: row)
+            {
+                element *= c; 
             }
         }
     }
 
-    return matrix;
+    return m_matrix;
 }
 
-Matrix algebra::sum(const Matrix& matrix, double c) {
-    Matrix new_matrix = matrix;
-    for(auto &row : new_matrix) {
-        for(auto &element : row) {
-            element += c;
-        }
-    }
-    return new_matrix;
-}
+Matrix algebra::multiply(const Matrix& matrix1, const Matrix& matrix2)
+{
+    Matrix m_matrix;
 
-Matrix algebra::sum(const Matrix& matrix1, const Matrix& matrix2) {
-    if(matrix1.empty() || matrix2.empty()) {
-        if(matrix1.empty() && matrix2.empty()) {
-            Matrix matrix;
-            return matrix;
-        }
-        throw std::logic_error("cannot add a non-empty matrix with an empty matrix");
+    if(matrix1.empty() || matrix2.empty())
+    {
+        if(matrix1.empty() && matrix2.empty()) return m_matrix;
+        else throw std::logic_error("A non-empty matrix cannot be multiplied with an empty matrix");
     }
-    if(matrix1.size() != matrix2.size() || matrix1[0].size() != matrix2[0].size()) {
-        throw std::logic_error("cannot add two different dimension matrixs");
-    }
-    Matrix new_matrix = matrix1;
-    int n = matrix1.size();
-    int m = matrix1[0].size();
-    for(int i = 0; i < n; ++i)
-        for(int j = 0; j < m; ++j) {
-            new_matrix[i][j] += matrix2[i][j];
-        }
-    return new_matrix;
-}
 
-Matrix algebra::transpose(const Matrix& matrix) {
-    if(matrix.empty()) {
-        Matrix matrix;
-        return matrix;
-    }
-    int n = matrix[0].size();
-    int m = matrix.size();
-    Matrix new_matrix(n, std::vector<double>(m));
-    for(int i = 0; i < n; ++i) {
-        for(int j = 0; j < m; ++j) {
-            new_matrix[i][j] = matrix[j][i];
-        }
-    }
-    return new_matrix;
-}
+    if(matrix1[0].size() == matrix2.size())
+    {
+        size_t n = matrix1.size();
+        size_t m = matrix2[0].size();
+        
+        m_matrix = Matrix(n, std::vector<double>(m, 0.0));
 
-Matrix algebra::minor(const Matrix& matrix, size_t n, size_t m) {
-    int row_num = matrix.size();
-    int column_num = matrix[0].size();
-    Matrix new_matrix;
-    std::vector<double> row;
-    for(int i = 0; i < row_num; ++i) {
-        if(i == n) continue;
-        row.clear();
-        for(int j = 0; j < column_num; ++j) {
-            if(j == m) continue;
-            row.push_back(matrix[i][j]);
-        }
-        new_matrix.push_back(row);
-    }
-    return new_matrix;
-}
-
-double algebra::determinant(const Matrix& matrix) {
-    if(matrix.empty()) { return 1; }
-    int row_num = matrix.size();
-    int column_num = matrix[0].size();
-    if(row_num != column_num) {
-        throw std::logic_error("cannot calculate a matrix with different row_num and column_num");
-    }
-    if(row_num == 1) {
-        return matrix[0][0];
-    }
-    if(row_num == 2) {
-        return matrix[0][0] * matrix[1][1] - matrix[0][1] * matrix[1][0];
-    }
-    double result = 0;
-    for(int i = 0; i < column_num; ++i) {
-        // calculate determinant using the first row
-        int sign = i % 2 == 0 ? 1 : -1;
-        result += sign * matrix[0][i] * determinant(minor(matrix, 0, i));
-    }
-    return result;
-}
-
-Matrix algebra::inverse(const Matrix& matrix) {
-    double matrix_determinant = determinant(matrix);
-    if(std::fabs(matrix_determinant) < __DBL_EPSILON__) {
-        throw std::logic_error("Matrix with determinant equals to zero doesn't have inverse");
-    }
-    int row_num = matrix.size();
-    Matrix new_matrix = zeros(row_num, row_num);
-    for(int i = 0; i < row_num; ++i) {
-        for(int j = 0; j < row_num; ++j) {
-            int sign = (i + j) % 2 == 0 ? 1 : -1;
-            double value = determinant(minor(matrix, i, j));
-            new_matrix[i][j] = sign * value / matrix_determinant;
-        }
-        std::cout << std::endl;
-    }
-    return transpose(new_matrix);
-}
-
-Matrix algebra::concatenate(const Matrix& matrix1, const Matrix& matrix2, int axis) {
-    if(matrix1.empty() || matrix2.empty()) {
-        if(matrix1.empty() && matrix2.empty()) {
-            Matrix matrix;
-            return matrix;
-        }
-        throw std::logic_error("cannot concatenate a non-empty matrix with an empty matrix");
-    }
-    Matrix matrix;
-    if(axis == 0) {
-        if(matrix1[0].size() != matrix2[0].size()) {
-            throw std::logic_error("cannot concatenate two matrix with different column number in axis 0");
-        }
-        for(auto row : matrix1) matrix.push_back(row);
-        for(auto row : matrix2) matrix.push_back(row);
-    } else {
-        if(matrix1.size() != matrix2.size()) {
-            throw std::logic_error("cannot concatenate two matrix with different row number in axis 1");
-        }
-        matrix.resize(matrix1.size(), std::vector<double>(matrix1[0].size() + matrix2[0].size()));
-        for(int i = 0; i < matrix1.size(); ++i) {
-            for(int j = 0; j < matrix1[0].size(); ++j) {
-                matrix[i][j] = matrix1[i][j];
-            }
-            for(int j = 0; j < matrix2[0].size(); ++j) {
-                matrix[i][j + matrix1[0].size()] = matrix2[i][j];
-            }
-        }
-    }
-    return matrix;
-}
-
-Matrix algebra::ero_swap(const Matrix& matrix, size_t r1, size_t r2) {
-    if(matrix.size() - 1 < (r1 < r2 ? r2 : r1)) {
-        throw std::logic_error("cannot swap a row whose row_number greater than matrix's size");
-    }
-    Matrix new_matrix = matrix;
-    swap(new_matrix[r1], new_matrix[r2]);
-    return new_matrix;
-}
-
-Matrix algebra::ero_multiply(const Matrix& matrix, size_t r, double c) {
-    if(matrix.size() - 1 < r) {
-        throw std::logic_error("cannot multiply a constant to a non-exist row");
-    }
-    Matrix new_matrix = matrix;
-    for(auto &elem : new_matrix[r])
-        elem *= c;
-    return new_matrix;
-}
-
-Matrix algebra::ero_sum(const Matrix& matrix, size_t r1, double c, size_t r2) {
-    if(matrix.size() - 1 < (r1 < r2 ? r2 : r1)) {
-        throw std::logic_error("cannot operate non-exist row");
-    }
-    Matrix new_matrix = matrix;
-    if(matrix.empty()) {
-        return new_matrix;
-    }
-    int m = matrix[0].size();
-    for(int i = 0; i < m; ++i) {
-        new_matrix[r2][i] += c * matrix[r1][i];
-    }
-    return new_matrix;
-}
-
-Matrix algebra::upper_triangular(const Matrix& matrix) {
-    Matrix new_matrix = matrix;
-    if(matrix.empty()) { return new_matrix; }
-    if(matrix.size() != matrix[0].size()) {
-        throw std::logic_error("cannot upper triangular a matrix with different row_num and column_num");
-    }
-    int n = matrix.size();
-    for(int i = 0; i < n - 1; ++i) {
-        if(std::fabs(matrix[i][i]) < __DBL_EPSILON__) {
-            bool swap_flag = false;
-            for(int j = i + 1; j < n; ++j) {
-                if(std::fabs(matrix[j][i]) >= __DBL_EPSILON__) {
-                    swap(new_matrix[i], new_matrix[j]);
-                    swap_flag = true;
-                    break;
+        for(size_t i = 0; i < n; ++i)
+        {
+            for(size_t j = 0; j < m; ++j)
+            {
+                for(size_t k = 0; k < matrix2.size(); ++k)
+                {
+                    m_matrix[i][j] += matrix1[i][k] * matrix2[k][j];
                 }
             }
-            if(!swap_flag) {
-                continue;
+        }
+
+        return m_matrix;
+    }
+    else throw std::logic_error("The number of columns of matrix1 is not equal to the number of rows of matrix2");   
+}
+
+Matrix algebra::sum(const Matrix& matrix, double c)
+{
+    Matrix m_matrix(matrix);
+
+    if(!matrix.empty())
+    {
+        for(std::vector<double> &row: m_matrix)
+        {
+            for(double &element: row)
+            {
+                element += c; 
             }
         }
-        for(int j = i + 1; j < n; ++j) {
-            double row_head = new_matrix[j][i];
-            for(int k = i; k < n; ++k) {
-                new_matrix[j][k] -= (row_head * new_matrix[i][k] / new_matrix[i][i]);
+
+        
+    }
+    
+    return m_matrix;
+}
+
+Matrix algebra::sum(const Matrix& matrix1, const Matrix& matrix2)
+{
+    Matrix m_matrix(matrix1);
+
+    if(matrix1.empty() || matrix2.empty())
+    {
+        if(matrix1.empty() && matrix2.empty()) return m_matrix;
+        else throw std::logic_error("A non-empty matrix cannot be added to an empty matrix");
+    }
+
+    size_t n = matrix1.size();
+    size_t m = matrix1[0].size();
+
+    if(n == matrix2.size() && m == matrix2[0].size())
+    {
+        for(size_t i = 0; i < n; ++i)
+        {
+            for(size_t j = 0; j < m; ++j)
+            {
+                m_matrix[i][j] += matrix2[i][j];
+            }
+        }
+
+        return m_matrix;
+    }
+    else throw std::logic_error("The number of rows and columns of matrix1 and matrix2 are not equal");
+}
+
+Matrix algebra::transpose(const Matrix& matrix)
+{
+    Matrix m_matrix;
+
+    if(!matrix.empty())
+    {
+        size_t n = matrix[0].size();
+        size_t m = matrix.size();
+
+        m_matrix = Matrix(n, std::vector<double>(m, 0.0));
+
+        for(size_t i = 0; i < n; ++i)
+        {
+            for(size_t j = 0; j < m; ++j)
+            {
+                m_matrix[i][j] = matrix[j][i];
             }
         }
     }
+
+    return m_matrix;
+}
+
+Matrix algebra::minor(const Matrix& matrix, size_t n, size_t m)
+{
+    if(matrix.empty()) throw std::logic_error("matrix is an empty matrix");
+
+    size_t row = matrix.size();
+    size_t column = matrix[0].size();
+    if(n >= row || m >= column) throw std::logic_error("The input n and m are wrong.");
+    
+    Matrix m_matrix(row - 1, std::vector<double>(column - 1, 0.0));
+    for(size_t m_i = 0, i = 0; i < row; ++i)
+    {
+        if(i != n)
+        {
+            for(size_t m_j = 0, j = 0; j < column; ++j)
+            {
+                if(j != m)
+                {
+                    m_matrix[m_i][m_j] = matrix[i][j];
+                    ++m_j;
+                }
+            }
+
+            ++m_i;
+        }
+    }
+
+    return m_matrix;
+}
+
+double algebra::determinant(const Matrix& matrix)
+{
+    if(matrix.empty()) return 1.0;
+
+    if(matrix.size() != matrix[0].size()) throw std::logic_error("The matrix is not square");
+    else
+    {
+        double value = 0.0;
+        int sign = 1;
+        for(size_t i = 0; i < matrix.size(); ++i)
+        {
+            (i % 2)? sign = -1: sign = 1;
+            value += (sign * matrix[i][0] * determinant(minor(matrix, i, 0)));
+        }
+        return value;
+    }
+}
+
+Matrix algebra::inverse(const Matrix& matrix)
+{
+    if(matrix.empty()) return matrix;
+
+    if(matrix.size() != matrix[0].size())
+    {
+        throw std::logic_error("This matrix does not have an inverse");
+    }
+
+    double det = determinant(matrix);
+    if(det == 0.0) throw std::logic_error("The determinant of this matrix is 0");
+
+    size_t n = matrix.size();
+    Matrix new_matrix(n, std::vector<double>(n, 0.0));
+    int sign = 1;
+    for(size_t i = 0; i < n; ++i)
+    {
+        for(size_t j = 0; j < n; ++j)
+        {
+            (i + j) % 2? sign = -1: sign = 1;
+            new_matrix[i][j] = sign * determinant(minor(matrix, j, i));
+        }
+    }
+
+    return multiply(new_matrix, 1 / det);
+}
+
+Matrix algebra::concatenate(const Matrix& matrix1, const Matrix& matrix2, int axis)
+{
+    if(axis != 0 && axis != 1) throw std::logic_error("Wrong axis value");
+
+    if(matrix1.empty() || matrix2.empty())
+    {
+        if(matrix1.empty()) return matrix2;
+        else return matrix1;
+    }
+
+    Matrix new_matrix;
+    if(axis)
+    {
+        if(matrix1.size() != matrix2.size()) 
+        {
+            throw std::logic_error("The two matrices have different numbers of rows and cannot be concatenated");
+        }
+
+        size_t row = matrix1.size();
+        size_t column = matrix1[0].size() + matrix2[0].size();
+        new_matrix = Matrix(row, std::vector<double>(column, 0.0));
+
+        for(size_t i = 0; i < row; ++i)
+        {
+            for(size_t j = 0; j < matrix1[0].size(); ++j)
+            {
+                new_matrix[i][j] = matrix1[i][j];
+            }
+
+            for(size_t j = 0; j < matrix2[0].size(); ++j)
+            {
+                new_matrix[i][j + matrix1[0].size()] = matrix2[i][j];
+            }
+        }
+    }
+    else
+    {
+        if(matrix1[0].size() != matrix2[0].size()) 
+        {
+            throw std::logic_error("The two matrices have different numbers of column and cannot be concatenated");
+        }
+
+        size_t row = matrix1.size() + matrix2.size();
+        size_t column = matrix1[0].size();
+        new_matrix = Matrix(row, std::vector<double>(column, 0.0));
+
+        for(size_t j = 0; j < column; ++j)
+        {
+            for(size_t i = 0; i < matrix1.size(); ++i)
+            {
+                new_matrix[i][j] = matrix1[i][j];
+            }
+
+            for(size_t i = 0; i < matrix2.size(); ++i)
+            {
+                new_matrix[i + matrix1.size()][j] = matrix2[i][j];
+            }
+        }
+    }
+
+    return new_matrix;
+}
+
+Matrix algebra::ero_swap(const Matrix& matrix, size_t r1, size_t r2)
+{
+    if(r1 >= matrix.size() || r2 >= matrix.size())
+    {
+        throw std::logic_error("r1 or r2 out of range");
+    }
+
+    if(matrix.empty()) return matrix;
+
+    Matrix new_matrix(matrix);
+
+    for(size_t j = 0; j < matrix[0].size(); ++j)
+    {
+        std::swap(new_matrix[r1][j], new_matrix[r2][j]);
+    }
+
+    return new_matrix;
+}
+
+Matrix algebra::ero_multiply(const Matrix& matrix, size_t r, double c)
+{
+    if(matrix.empty()) return matrix;
+
+    if(r >= matrix.size())
+    {
+        throw std::logic_error("r out of range");
+    }
+
+    Matrix new_matrix(matrix);
+
+    for(size_t j = 0; j < matrix[0].size(); ++j)
+    {
+        new_matrix[r][j] *= c;
+    }
+
+    return new_matrix;
+}
+
+Matrix algebra::ero_sum(const Matrix& matrix, size_t r1, double c, size_t r2)
+{
+    if(matrix.empty()) return matrix;
+
+    if(r1 >= matrix.size() || r2 >= matrix.size())
+    {
+        throw std::logic_error("r1 or r2 out of range");
+    }
+
+    Matrix new_matrix(matrix);
+    for(size_t j = 0; j < matrix[0].size(); ++j)
+    {
+        new_matrix[r2][j] += (matrix[r1][j] * c);
+    }
+    return new_matrix;
+}
+
+Matrix algebra::upper_triangular(const Matrix& matrix)
+{
+    if(matrix.empty()) return matrix;
+
+    if(matrix.size() != matrix[0].size()) throw std::logic_error("The matrix is not square");
+    
+    Matrix new_matrix(matrix);
+    size_t n = matrix.size();
+    for(size_t i = 0; i < n; ++i) // Handling the case where there are 0 on the main diagonal
+    {
+        if(new_matrix[i][i] == 0)
+        {
+            size_t n_i = i + 1;
+            while(n_i < n && new_matrix[n_i][n_i] == 0) ++n_i;
+
+            if(n_i < n)
+            {
+                new_matrix = ero_swap(new_matrix, i, n_i);
+            }
+        }
+    }
+
+    double c;
+    for(size_t i = 0; i < n; ++i)
+    {
+        for(size_t n_i = i + 1; n_i < n; ++n_i)
+        {
+            c = -(new_matrix[n_i][i] / new_matrix[i][i]);
+            new_matrix = ero_sum(new_matrix, i, c, n_i);
+        }
+    }
+
     return new_matrix;
 }
--- Answer/HW1/hw1.h
+++ Answer/HW1/hw1.h
@@ -1,49 +1,44 @@
 #ifndef AP_HW1_H
 #define AP_HW1_H
+
 #include <vector>
+#include <random>
 #include <iostream>
 #include <iomanip>
-#include <cassert>
-#include <cmath>
-#include <random>
+#include <utility>
 
-using Matrix = std::vector<std::vector<double> >;
+// The role of using here is the same as typedef in C language
+using Matrix = std::vector<std::vector<double>>;
 
-class algebra {
+class algebra
+{
+    // NOTE: Because of unit_test, the member functions here must be declared as static before they can be called
+public:
+    static Matrix zeros(size_t n, size_t m);
+    static Matrix ones(size_t n, size_t m);
+    // NOTE:Use the random library to implement
+    static Matrix random(size_t n, size_t m, double min, double max);
+    // NOTE:Use the iomanip library to implement
+    static void show(const Matrix& matrix);
+    static Matrix multiply(const Matrix& matrix, double c);
+    static Matrix multiply(const Matrix& matrix1, const Matrix& matrix2);
+    // NOTE:Add c to each element in the matrix
+    static Matrix sum(const Matrix& matrix, double c);
+    static Matrix sum(const Matrix& matrix1, const Matrix& matrix2);
+    static Matrix transpose(const Matrix& matrix);
+    static Matrix minor(const Matrix& matrix, size_t n, size_t m);
+    static double determinant(const Matrix& matrix);
+    static Matrix inverse(const Matrix& matrix);
+    // NOTE:axis=0: on top of each other | axis=1: alongside each other
+    static Matrix concatenate(const Matrix& matrix1, const Matrix& matrix2, int axis=0);
 
-    public:
-        static Matrix zeros(size_t n, size_t m);
+    // INFO:The following are the basic line operations
+    static Matrix ero_swap(const Matrix& matrix, size_t r1, size_t r2);
+    static Matrix ero_multiply(const Matrix& matrix, size_t r, double c);
+    static Matrix ero_sum(const Matrix& matrix, size_t r1, double c, size_t r2);
 
-        static Matrix ones(size_t n, size_t m);
-
-        static Matrix random(size_t n, size_t m, double min, double max);
-
-        static void show(const Matrix& matrix);
-
-        static Matrix multiply(const Matrix& matrix, double c);
-
-        static Matrix multiply(const Matrix& matrix1, const Matrix& matrix2);
-
-        static Matrix sum(const Matrix& matrix, double c);
-
-        static Matrix sum(const Matrix& matrix1, const Matrix& matrix2);
-
-        static Matrix transpose(const Matrix& matrix);
-
-        static Matrix minor(const Matrix& matrix, size_t n, size_t m);
-
-        static double determinant(const Matrix& matrix);
-
-        static Matrix inverse(const Matrix& matrix);
-
-        static Matrix concatenate(const Matrix& matrix1, const Matrix& matrix2, int axis = 0);
-
-        static Matrix ero_swap(const Matrix& matrix, size_t r1, size_t r2);
-
-        static Matrix ero_multiply(const Matrix& matrix, size_t r, double c);
-
-        static Matrix ero_sum(const Matrix& matrix, size_t r1, double c, size_t r2);
-
-        static Matrix upper_triangular(const Matrix& matrix);
+    static Matrix upper_triangular(const Matrix& matrix);
+    
 };
+
 #endif //AP_HW1_H
